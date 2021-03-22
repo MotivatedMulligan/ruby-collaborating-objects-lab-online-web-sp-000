2fe422f69653cec1a8b1aeda6baac8511b988e7a
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-22 19:08:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,13 @@
 class Song
   attr_accessor :name, :artist
-
+@@all = []
   def initialize(name)
     @name = name
   end
-
+def artist=
+    song.artist = artist
+end
+#########
   def self.new_by_filename(filename)
     split_name = filename.split(" - ")
     song = self.new(split_name[1])
@@ -12,13 +15,19 @@ class Song
     song.artist = artist
     artist.add_song(song)
     song
+    #associates new song instance with
+    #artist from the filename
   end
+
  def self.all
    @@all #returns all existing Song instances
  end
 
  def artists_name=(artist)
    @artist_name = artist
+   #accepts artist's name, finds/creates
+   #Artist instance and assigns to
+   #Song's artist attribute
  end
- 
+
 end
