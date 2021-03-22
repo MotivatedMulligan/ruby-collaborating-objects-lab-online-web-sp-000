a7a03b12177ac7bd41da990a3daf3ca361afcdf1
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-22 19:46:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,30 +10,23 @@ class Song
 def self.all
   @@all
 end
-#########
 def artist=(artist)
     @artist = artist
     artist.songs << self
 end
 
    def self.new_by_filename(filename)
-    #  artist_name, song_name = filename.split(" - ")
-    #  song = Song.new(song_name)
-    #  song.artist_name = artist_name
-    #  song
     split_name = filename.split(" - ")
     song = self.new(split_name[1])
     artist = Artist.find_or_create_by_name(split_name[0])
     song.artist = artist
     artist.add_song(song)
     song
-    #associates new song instance with
-    #artist from the filename
   end
-
+#########
  def artist_name=(artist)
   #if
-   artist.name = Artist.find_or_create_by_name(name)
+   self.artist = Artist.find_or_create_by_name(name)
    #nil
   #else
   # artist
