e00f5c505a2d377f125cf9713b42078c082bf2b7
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-22 19:27:56 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,10 +1,25 @@
 class MP3Importer
   attr_accessor :path
-  def initialize(filepath)
-    @path = filepath
+  
+  def initialize(path)
+    @path = path
+  #  @library = []
   end
+  ##########
   def files
-    @files = Dir.entries(path)
-    @files.delete_if {|file| file == "." || file = ".."}
+    Dir.entries(self.path)[2..5]
+  #!  @files = Dir.entries(path)
+#!  @files.delete_if {|file| file == "." || file = ".."}
+#loads all the mp3 files in the path directory
+#normalizes the filename to just the mp3 
+#filename with no path
   end
+  
+  def import
+    #! @library << files
+    files.each do |filename| Song.new_by_filename(filename)
+    #imports files into 
+    #library by creating songs from a filename
+  end
+  
 end
