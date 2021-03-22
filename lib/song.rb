981890ad495ee382e8d34405c9af89ef608fd26b
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-22 20:04:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,7 @@ end
     song
   end
 #########
- def artist_name=(name)
+ def artist_name=(Artist)
   #if
   # self.artist = Artist.find_or_create_by_name(name)
    Artist.all.detect {|artist| artist.name == name } || Artist.new(name)
