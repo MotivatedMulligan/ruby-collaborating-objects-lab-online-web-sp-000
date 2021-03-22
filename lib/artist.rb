cb149a22946042cb262c131ee96b0dabd5de98f7
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-22 18:47:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+class Artist
+
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def name=(name)
+    @name = (name)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def self.find_or_create_by_name(name)
+    self.all.detect {|artist| cartist.name == name } || Artist.new(name)
+  end
+
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
+end
