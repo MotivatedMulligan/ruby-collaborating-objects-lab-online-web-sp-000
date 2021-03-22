dde2c333867a039ad987406f68d35c08a2a0bf52
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-22 20:01:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,11 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
+  attr_reader :songs
   @@all = []
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
+    #@@all << self
   end
   def name=(name)
     @name = (name)
